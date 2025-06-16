ae1284cd20ec77946a39da04474829f853e25a9b
User Name <Email@example.com>
2025-06-16 04:27:13 +0300

    moive website

--- src/redux/Slice/moviesSlice.js
+++ src/redux/Slice/moviesSlice.js
@@ -2,7 +2,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = "http://localhost:3000/movies";
+const API_URL = "http://localhost:10000/movies";
 
 
 export const fetchMovies = createAsyncThunk('movies/fetchMovies', async () => {
